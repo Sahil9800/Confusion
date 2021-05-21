02d5a67c2113bc11fa1d0271538c785636ceea4f
Sahil <schoudhary@es.iitr.ac.in>
2021-05-21 05:30:39 +0530

    Uncontrolled Forms

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -25,6 +25,7 @@ class Contact extends Component {
         this.handleSubmit=this.handleSubmit.bind(this);
         this.handleInputChange=this.handleInputChange.bind(this);
         this.handleBlur=this.handleBlur.bind(this);
+        this.validate=this.validate.bind(this);
     }
 
     handleInputChange(event) {
